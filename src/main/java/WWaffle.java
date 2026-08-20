b6d97fe687f2375b7c5780fd57a350f43d726d2f
Chen Sixian <e1716280@u.nus.edu>
2026-08-21 02:56:43 +0800

    Add command echo loop for Level 1

--- src/main/java/WWaffle.java
+++ src/main/java/WWaffle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class WWaffle {
     public static void main(String[] args) {
         String banner = """
@@ -5,9 +7,24 @@ public class WWaffle {
         ║        WWAFFLE         ║
         ╚════════════════════════╝
         """;
+
+        String line = "____________________________________________________________";
         System.out.println(banner);
+        System.out.println(line);
         System.out.println("Hello! I'm WWaffle");
         System.out.println("What can I do for you?");
+        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(line);
+            System.out.println(command);
+            System.out.println(line);
+            command = scanner.nextLine();
+        }
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
